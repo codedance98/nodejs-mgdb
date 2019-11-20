25212c022475188c1a889257b1b84f140c1c1028
李良奇 <codedance98@gmail.com>
2019-11-20 19:13:03 +0800

    update

--- api/v1.js
+++ api/v1.js
@@ -3,7 +3,8 @@ const db = require('mongoose');
 let api = express.Router();
 const UserModel = require("../db/model/user.js");
 const db_add = 'mongodb+srv://codedance98:liliangqi1@cluster0-cjz8o.mongodb.net/test?retryWrites=true&w=majority';
-
+// gui: https://huyinjie.xyz/2018/05/05/%E5%8F%AF%E8%A7%86%E5%8C%96%E7%AE%A1%E7%90%86MongoDB-Atlas/
+// ip check: https://www.toolsdaquan.com/ipcheck/
 api.post("/create", function(req, res) {
     let t = {
         name:req.body.name,
